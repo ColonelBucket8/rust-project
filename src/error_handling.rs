fa581ba4794d80c9f7d993287077b05b657cdab3
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2022-10-22 07:40:29 +0800

    added recoverable errors with result

--- src/error_handling.rs
+++ src/error_handling.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io::{self, ErrorKind, Read};
 
 pub fn error_handling() {
     let greeting_file_result = File::open("hello.txt");
@@ -18,6 +18,27 @@ pub fn error_handling() {
     };
 
     println!("File open {:?}", greeting_file);
+
+    // shortcurts for panic on error
+
+    let greeting_file_2 = File::open("hello.txt").unwrap();
+
+    // most Rustaceans choose expect rather than unwrap and give more context
+    // about why the operation is expected to always succeed
+    let greeting_file_3 =
+        File::open("hello.txt").expect("hello.txt should be included in this project");
+
+    let from_username = read_username_from_file("test.txt");
+
+    println!("read username from file {:?}", from_username);
+
+    let from_username_2 = read_username_from_file_v2();
+
+    println!("read username from file v2 {:?}", from_username_2);
+
+    let from_username_3 = read_username_from_file_v3();
+
+    println!("read username from file v3 {:?}", from_username_3);
 }
 
 // Using closures and if else
@@ -33,4 +54,42 @@ fn open_file() {
     });
 
     println!("greeting file {:?}", greeting_file);
+}
+
+fn read_username_from_file(file_name: &str) -> Result<String, io::Error> {
+    let username_file_result = File::open(file_name);
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(error) => return Err(error),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(error) => Err(error),
+    }
+}
+
+fn read_username_from_file_v2() -> Result<String, io::Error> {
+    let mut username_file = File::open("ihello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file_v3() -> Result<String, io::Error> {
+    let mut username = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+
+    Ok(username)
+
+    // shorter way
+    // fs::read_to_string("hello.txt")
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
 }

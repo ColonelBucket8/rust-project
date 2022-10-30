8f328b955f8717b773d70eb3925c8d9c05ab245a
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2022-10-30 17:04:41 +0800

    added running code on cleanup with the drop trait

--- src/smart_pointers.rs
+++ src/smart_pointers.rs
@@ -1,13 +1,43 @@
+use crate::smart_pointers::List::{Cons, Nil};
+
 enum List {
     Cons(i32, Box<List>),
     Nil,
 }
 
-use crate::smart_pointers::List::{Cons, Nil};
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
 
 pub fn smart_pointers() {
     let b = Box::new(5);
     println!("b = {}", b);
 
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    // Rust automatically called drop when instances went out of scope
+    // Variable are dropped in the reverse order of their creation
+    // so d was dropped before c
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointer created.");
+
+    // Dropping a value early with std::mem::drop
+
+    let e = CustomSmartPointer {
+        data: String::from("some data"),
+    };
+    println!("CustomSmartPointer created.");
+    drop(e);
+    println!("CustomSmartPointer dropped before the end of main.");
 }

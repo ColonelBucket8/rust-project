5828cb438a51d63e3b40d2872581b809f3f6c527
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2022-10-30 18:32:07 +0800

    added Rc^<T^> the reference counted smart pointer

--- src/smart_pointers.rs
+++ src/smart_pointers.rs
@@ -1,8 +1,15 @@
 use crate::smart_pointers::List::{Cons, Nil};
+use crate::smart_pointers::ListV2::{ConsV2, NilV2};
+use std::rc::Rc;
 
 enum List {
     Cons(i32, Box<List>),
     Nil,
+}
+
+enum ListV2 {
+    ConsV2(i32, Rc<ListV2>),
+    NilV2,
 }
 
 struct CustomSmartPointer {
@@ -40,4 +47,14 @@ pub fn smart_pointers() {
     println!("CustomSmartPointer created.");
     drop(e);
     println!("CustomSmartPointer dropped before the end of main.");
+
+    let x = Rc::new(ConsV2(5, Rc::new(ConsV2(10, Rc::new(NilV2)))));
+    println!("count after creating x = {}", Rc::strong_count(&x));
+    let y = ConsV2(3, Rc::clone(&x));
+    println!("count after creating y = {}", Rc::strong_count(&x));
+    {
+        let z = ConsV2(4, Rc::clone(&x));
+        println!("count after creating z = {}", Rc::strong_count(&x));
+    }
+    println!("count after z goes out of scope = {}", Rc::strong_count(&x));
 }

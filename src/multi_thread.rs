ff472abfef3c9991795c75c34fb16f38fa7ef78f
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2022-11-05 12:11:12 +0800

    finished on multi thread

--- src/multi_thread.rs
+++ src/multi_thread.rs
@@ -13,4 +13,34 @@ pub fn multi_thread() {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    join_handles();
+    move_closures();
+}
+
+fn join_handles() {
+    println!("start join handles");
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap();
+}
+
+fn move_closures () {
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle.join().unwrap();
 }
